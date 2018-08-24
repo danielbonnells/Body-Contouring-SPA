9f1bcdc4e6b10355ef20fbb896431d4641aa0849
Daniel <danielbonnells1@gmail.com>
2018-08-24 07:09:00 -0400

    changing slider

--- spa-template/spa-template/src/slider.js
+++ spa-template/spa-template/src/slider.js
@@ -1,115 +1,53 @@
 import React, { Component } from 'react'
 import { Slide } from './slide'
-
+import { TextBlock } from './textblock'
 export class Slider extends Component {
 
     constructor(props){
         super(props)
 
         this.state = {
-            currentIndex: 3,
-            zindex: [4, 5, 6]
+            currentIndex: 0,
+            slides: [0,1,2],
+            odd: true
         }
-
-    
     }
-    changeIndex = (slide) => {
-        console.log("slide index was " + slide.style.zIndex)
-        // if(parseInt(slide.style.zIndex, 10) === 1){
-        //     slide.style.zIndex = 4
-        // } else if(parseInt(slide.style.zIndex, 10) === 2){
-        //     slide.style.zIndex = 5
-        // } else if(parseInt(slide.style.zIndex, 10) === 3){
-        //     slide.style.zIndex = 6
-        // }
-
-        switch(parseInt(slide.style.zIndex, 10)){
-            case 1:
-                slide.style.zIndex = 4;
-                break;
-            case 2:
-                slide.style.zIndex = 5;
-                break;
-            case 3:
-                slide.style.zIndex = 6;
-                break;
-            case 4:
-                slide.style.zIndex = 1;
-                break;
-            case 5:
-                slide.style.zIndex = 2;
-                break;   
-            case 6:
-                slide.style.zIndex = 3;
-                break; 
-        }
-
-        slide.style.left = "0px"
-        //slide.style.zIndex = slide.style.zIndex - 3
-        console.log("slide index is now " + slide.style.zIndex)
-    }
-    
-    changeSlide = () => {
-
-        if(this.state.currentIndex === 1){
-            console.log("hello")
-            this.setState(prevState => ({
-                currentIndex: prevState.currentIndex + 3
-            }))
-        }
-
-        let slide = document.getElementById(`slide${this.state.currentIndex}`)
-
-        console.log(slide.id)
-
-        slide.style.left = "-100%"
-
-        setTimeout(this.changeIndex, 1000, slide)
-
-        console.log(this.state.currentIndex)
-
-        this.setState(prevState => ({
-            currentIndex: prevState.currentIndex - 1
-        }))
-    }   
-
-   
 
     render(){
         
         const images = [
             {
+                id: 0,
+                source: "https://www.cstatic-images.com/car-pictures/xl/USC70CHC021E021001.jpg",
+                alt: "",
+                z: 3
+            },
+            {
                 id: 1,
-                source: "https://picsum.photos/1920/1080?random",
-                alt: ""
+                source: "https://cms.kelleybluebookimages.com/content/dam/kbb-editorial/make/acura/nsx/2017/first-review/01-2017-acura-nsx.jpg",
+                alt: "",
+                z: 2
             },
             {
                 id: 2,
-                source: "https://picsum.photos/1920/1080?random",
-                alt: ""
-            },
-            {
-                id: 3,
-                source: "https://picsum.photos/1920/1080?random",
-                alt: ""
+                source: "https://media.wired.com/photos/59b1a378a0df4b47dcf7ccb0/master/w_2400,c_limit/LamborghiniRoadsterTA.jpg",
+                alt: "",
+                z: 1
             }
         ]
 
         const slides = images.map((image) => {
-            return (<Slide source={image.source} id={`slide${image.id}`} alt={image.alt} key={image.id} zindex={this.state.zindex[image.id - 1]} next={this.changeSlide}/>)
+            return (<Slide source={image.source} id={image.id} alt={image.alt} key={image.id} zindex={image.z} next={this.getSlides}/>)
         })   
 
 
 
         return (
             <div className="slide-container">
-            {slides}
-                <div className="home-text-container">
-                <h1>Title Here</h1>
-                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
-                <button>CTA</button>
-            </div> 
-                
+                <div className="slides-box">
+                    {slides}
+                </div>
+                <TextBlock />
             </div>
             
         )
